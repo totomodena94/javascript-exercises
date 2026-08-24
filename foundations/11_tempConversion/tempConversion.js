0d839205b641ca74eb6eb6d59398b0c64f50f4b6
toto.modena94 <295842057+totomodena94@users.noreply.github.com>
2026-08-24 14:57:19 +0200

    Complete exercise tempConversion and fixed string to number problem

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) *5/9;
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
+
+
+
+
+convertToCelsius(32);
 
 // Do not edit below this line
 module.exports = {
